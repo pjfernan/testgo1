5dca4dc652c51e3a0b5fb933bbbef956bc65ec21
Pablo Fernandez <pjfernan@Pablos-MacBook-Pro.local>
2018-12-23 21:55:16 -0300

    mayusculas

--- tesgo1.go
+++ tesgo1.go
@@ -1,6 +1,6 @@
 package main
 import "fmt"
 func main() {
-	fmt.Printf("hola a todos los participantes.\n Prueba de salto de linea. \n")
+	fmt.Printf("Hola a todos los participantes.\n Prueba de salto de línea. \n")
 	fmt.Printf("Estamos probando GoLang.")
 }
