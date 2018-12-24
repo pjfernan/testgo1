919f0a54dc50d75424bd03fab54286aae7aa94c3
Pablo Fernandez <pjfernan@Pablos-MacBook-Pro.local>
2018-12-23 21:47:28 -0300

    diferenciarlo de github

--- tesgo1.go
+++ tesgo1.go
@@ -2,5 +2,5 @@ package main
 import "fmt"
 func main() {
 	fmt.Printf("hola a todos.\n Prueba de salto de linea. \n")
-	fmt.Printf("Estamos probando Go!.")
+	fmt.Printf("Estamos probando GoLang.")
 }
