ed367aead61aab449bfbe69adc13b0015635b395
Pablo Fernandez <pjfernan@Pablos-MacBook-Pro.local>
2018-12-23 21:50:02 -0300

    staged

--- tesgo1.go
+++ tesgo1.go
@@ -1,6 +1,6 @@
 package main
 import "fmt"
 func main() {
-	fmt.Printf("hola a todos.\n Prueba de salto de linea. \n")
+	fmt.Printf("hola a todos los participantes.\n Prueba de salto de linea. \n")
 	fmt.Printf("Estamos probando GoLang.")
 }
